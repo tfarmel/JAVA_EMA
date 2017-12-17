581964d8133584c7e34e747b9123bc179a0162fb
tfarmel <tfarmel@yahoo.fr>
2017-12-17 17:37:32 +0100

    triangle de pascal

---
+++ src/com/tfarmel/basesAlgo2/TrianglePascal.java
@@ -0,0 +1,12 @@
+package com.tfarmel.basesAlgo2;
+
+import java.util.Scanner;
+
+public class TrianglePascal {
+	
+	public static void main(String[] args) {
+		System.out.println("Triangle de Pascal");
+		Scanner sc = new Scanner(System.in);
+	}
+
+}
